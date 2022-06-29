1e478e4494bcc7ef87c4bb6ee4657cdd7dd2c069
Dmitry <ed@mstagency.ru>
2022-06-29 09:32:55 +0400

    queue changes

--- src/Controller/Task/TaskController.php
+++ src/Controller/Task/TaskController.php
@@ -4,13 +4,19 @@ namespace App\Controller\Task;
 
 use App\Entity\Task;
 use App\Entity\User;
+use App\Message\TaskMessage;
 use App\Repository\TaskRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use function PHPUnit\Framework\isInstanceOf;
 
 /**
  * Class TaskController
@@ -47,11 +53,12 @@ class TaskController extends AbstractController
     /**
      * @param Request $request
      * @param Session $session
-     * @param UserRepository $userRepository
+     * @param MessageBusInterface $bus
+     * @param ManagerRegistry $doctrine
      * @return Response
      * @throws \Exception
      */
-    public function createTask(Request $request, Session $session, UserRepository $userRepository): Response
+    public function createTask(Request $request, Session $session): Response
     {
         $fields = ['title'];
         $errors = [];
@@ -62,7 +69,7 @@ class TaskController extends AbstractController
         if (!empty($errors))
             return $this->redirectToRoute('task_new', ['errors' => $errors]);
 
-        $user = $userRepository->find($this->getUser()->getId());
+        $user = $this->getUser();
 
         $task = new Task();
         $task->setTitle($request->get('title'));
